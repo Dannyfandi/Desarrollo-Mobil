f17f453aee4e55aedf3a4298ef3d50d2e3309dd9
Dannyfandi <daniel.fandino@urosario.edu.co>
2024-02-08 19:14:14 -0500

    Introduccion

---
+++ app/src/main/java/com/example/proyect1/Introduccion.kt
@@ -0,0 +1,21 @@
+package com.example.proyect1
+
+class Introduccion {
+}
+
+fun main() {
+    print("Hola")
+    println("Hola tonotos")
+
+    val name:String="Daniel"
+
+    println("el nombre es $name \n")
+
+    println("Ingrese su edad")
+    var edad= readLine()!!.toInt()
+    println("Ingrese su apellido")
+    var apellido= readLine()!!.toString()
+    println("La edad es $edad \n")
+    println("El apellido es $apellido \n")
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "Proyect 1"
 include(":app")
+include(":app")
+ 
